f998083e2dc3dc0b78c4e9adce782cb05cf8314f
jaykumarvprajapati-inventyv <jaykumar.v,2833@gmail.com>
2025-07-14 18:37:35 +0530

    Conflict solved

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 fn main() {
     println!("Hello, India walo!");
+
+    let client_name = String::from("Yash Patel");
+    println!("Name of my client is:-{}", client_name);
     println!("Myself Jay from Clone");
 }
